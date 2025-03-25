91b6b3e83244b9ad7a894febb0dbf750e5b2e441
Jacob Brewer <Jacob.brewer@bthree.uk>
2025-03-25 10:36:38 +0000

    adding missing mage cmd

---
+++ magefiles/deps.go
@@ -0,0 +1,20 @@
+package magefiles
+
+import (
+	"fmt"
+
+	"github.com/magefile/mage/sh"
+)
+
+func InstallDeps() error {
+	fmt.Println("Installing Deps...")
+	if err := sh.Run("go", "install", "github.com/jacobbrewer1/goschema@latest"); err != nil {
+		return fmt.Errorf("failed to install goschema: %w", err)
+	}
+
+	if err := sh.Run("go", "install", "golang.org/x/tools/cmd/goimports@latest"); err != nil {
+		return fmt.Errorf("failed to install goimports: %w", err)
+	}
+
+	return nil
+}
