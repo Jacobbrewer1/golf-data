67d0917887662772a5887d11465f709dd1c3866c
Jacob Brewer <Jacob.brewer@bthree.uk>
2025-04-12 22:37:43 +0100

    removing vault client override

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -15,11 +15,9 @@ import (
 	apiSvc "github.com/jacobbrewer1/golf-data/pkg/services/api"
 	"github.com/jacobbrewer1/golf-data/pkg/services/api/domain"
 	"github.com/jacobbrewer1/uhttp"
-	"github.com/jacobbrewer1/vaulty"
 	"github.com/jacobbrewer1/web"
 	"github.com/jacobbrewer1/web/health"
 	"github.com/jacobbrewer1/web/logging"
-	"github.com/spf13/viper"
 )
 
 const (
@@ -31,23 +29,6 @@ func main() {
 		logging.WithDefaultLogger(),
 		logging.WithAppName(appName),
 	)
-
-	web.VaultClient = func(ctx context.Context, l *slog.Logger, v *viper.Viper) (vaulty.Client, error) {
-		addr := v.GetString("vault.address")
-
-		vc, err := vaulty.NewClient(
-			vaulty.WithContext(ctx),
-			vaulty.WithAddr(addr),
-			vaulty.WithTokenAuth(v.GetString("vault.token")),
-			vaulty.WithKvv2Mount(v.GetString("vault.kvv2_mount")),
-			vaulty.WithLogger(l),
-		)
-		if err != nil {
-			return nil, fmt.Errorf("error creating vault client: %w", err)
-		}
-
-		return vc, nil
-	}
 
 	a, err := web.NewApp(l)
 	if err != nil {
